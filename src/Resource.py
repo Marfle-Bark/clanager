d03fcb6eb6b4101f5d7de9b032be87ae31036194
Marfle-Bark <ethan.busbee@gmail.com>
2014-12-23 19:22:05 +0800

    In Resource class, wrote accessors and harvest() method.

--- src/Resource.py
+++ src/Resource.py
@@ -8,8 +8,8 @@ class Resource(object):
 
   def __init__(self, kind, amount):
     try:
-      kind = str(kind)
-      amount = int(amount)
+      self._kind = str(kind)
+      self._amount = int(amount)
     except:
       print "\n\n" + str(traceback.format_exc())
       print "\n\nDATA VALIDATION ERROR"
@@ -18,3 +18,21 @@ class Resource(object):
   def __del__(self):
     self._kind = "DELETED"
     self._amount = 0
+
+  def _getHarvestAmount(self):
+    return min(self._amount, 5)
+
+  def getKind(self):
+    return self._kind
+
+  def getAmount(self):
+    return self._amount
+
+  def harvest(self):
+    if self._amount < 1:
+      harvestAmount = 0
+    else:
+      harvestAmount = self._getHarvestAmount()
+
+    self._amount = self._amount - harvestAmount
+    return (self._kind, amount)
