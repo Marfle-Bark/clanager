d717ae7757fc4e478dddb170313826bfd805e6d5
Marfle-Bark <ethan.busbee@gmail.com>
2014-12-23 19:41:24 +0800

    Added EXTREMELY basic stubbing (init + del only) to Person and Clan classes.

--- src/Clan.py
+++ src/Clan.py
@@ -1,3 +1,13 @@
 # Ethan Busbee
 # Project Clanager -- Clan Class
 # File creation: 2014-12-23
+
+import traceback
+
+class Clan(object):
+
+  def __init__(self):
+    pass
+
+  def __del__(self):
+    pass
--- src/Person.py
+++ src/Person.py
@@ -1,3 +1,13 @@
 # Ethan Busbee
 # Project Clanager -- Person Class
 # File creation: 2014-12-23
+
+import traceback
+
+class Person(object):
+
+  def __init__(self):
+    pass
+
+  def __del__(self):
+    pass
