8daefb9358dabbb7fd62c23109aedf4ae0608abd
Marfle-Bark <ethan.busbee@gmail.com>
2014-12-23 18:54:59 +0800

    Wrote basic data validation for Resource init function.

--- src/Resource.py
+++ src/Resource.py
@@ -2,15 +2,19 @@
 # Project Clanager -- Resource Class
 # File creation: 2014-12-23
 
+import traceback
+
 class Resource(object):
 
-  def __init__(self, kind = "NONE", amount = 0):
-    self._checkInputs(kind = kind, amount = amount)
+  def __init__(self, kind, amount):
+    try:
+      kind = str(kind)
+      amount = int(amount)
+    except:
+      print "\n\n" + str(traceback.format_exc())
+      print "\n\nDATA VALIDATION ERROR"
+      return
 
   def __del__(self):
     self._kind = "DELETED"
     self._amount = 0
-
-  def _checkInputs(self, kind, amount):
-    print "kind = " + kind
-    print "amount = " + amount
