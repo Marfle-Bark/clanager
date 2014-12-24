171c95cdbf4ef785d9e87011a801e217beb3431f
Marfle-Bark <ethan.busbee@gmail.com>
2014-12-24 15:35:26 +0800

    Implemented init and del for Area class.

--- src/Area.py
+++ src/Area.py
@@ -8,3 +8,33 @@ from Clan import *
 from Person import *
 from Resrouce import *
 
+
+class Area(object):
+
+  ID = 1
+
+  def __init__(self, name = None):
+    self._people = {}
+    self._resources = []
+    self._connections = {}
+    self._owner = None
+
+    self._ID = ID
+    ID = ID + 1
+
+    if name is not None:
+      try:
+        name = str(name)
+        self._name = name
+      except:
+        print "\n\n" + str(traceback.format_exc())
+        print "\n\nDATA VALIDATION ERROR: Area not created!"
+        return
+
+  def __del__(self):
+    self._people = {}
+    self._resources = []
+    self._connections = {}
+    self._owner = None
+    self._ID = 0
+    self._name = "DELETED"
