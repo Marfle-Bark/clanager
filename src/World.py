5aac1959c47b13af29cbd5880be2b93224e1e625
Marfle-Bark <ethan.busbee@gmail.com>
2014-12-24 18:45:27 +0800

    Built absolute basics of World class.
    Stubbed out init and del methods with pass.

---
+++ src/World.py
@@ -0,0 +1,16 @@
+# Ethan Busbee
+# Project Clanager -- World Class (lol)
+# File creation: 2014-12-24
+
+import traceback
+
+from Area import *
+
+
+class World(object):
+
+  def __init__(self):
+    pass
+
+  def __del__(self):
+    pass
