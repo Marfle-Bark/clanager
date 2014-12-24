49de201775779216eb38ad45ad687d7363272c5d
Marfle-Bark <ethan.busbee@gmail.com>
2014-12-24 12:07:34 +0800

    In Person class, wrote very basic init and del methods.

--- src/Person.py
+++ src/Person.py
@@ -3,11 +3,27 @@
 # File creation: 2014-12-23
 
 import traceback
+from Clan import Clan
+from Resource import Resource
 
 class Person(object):
 
-  def __init__(self):
-    pass
+  def __init__(self, name):
+    try:
+      self._name = str(name)  # Name of Person
+      self._clan = None       # Clan Person belongs to
+      self._needs = {}        # Person's needs (begins empty)
+      self._energy = 100      # Person's energy (low = hungry)
+      self._health = 100      # Person's health (low = dying)
+      self._alive = True      # Whether Person is alive or not
+    except:
+      print "\n\n" + str(traceback.format_exc())
+      print "\n\nDATA VALIDATION ERROR: Person not created!"
 
   def __del__(self):
-    pass
+    self._name = "DELETED"
+    self._clan = None
+    self._needs = {}
+    self._energy = 0
+    self._health = 0
+    self._alive = False
